d255600d581af50abce2fadb4457a96d9b447dc2
Resi Respati <resir014@gmail.com>
2020-11-08 23:03:06 +0700

    add link to GH repo

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,19 @@
-import { Flex, Text } from '@chakra-ui/core'
+import { Flex, Link, Text } from '@chakra-ui/core'
 import * as React from 'react'
 
 const Navigation: React.FC = () => (
   <Flex as="footer" flexDirection="column" px={6} py={3}>
-    <Text fontSize="sm">TMViz &copy; 2020 resir014. Released as open-source under the MIT license.</Text>
+    <Text fontSize="sm">
+      TMViz &copy; 2020 resir014. Released as{' '}
+      <Link href="https://github.com/resir014/TMViz" isExternal>
+        open-source
+      </Link>{' '}
+      under the{' '}
+      <Link href="https://github.com/resir014/TMViz/blob/trunk/LICENSE" isExternal>
+        MIT license
+      </Link>
+      .
+    </Text>
   </Flex>
 )
 
