13d9b9245e063906d9ab6c55b6cc95eb47e6925d
Resi Respati <resir014@gmail.com>
2020-11-09 22:52:02 +0700

    handle empty input text

--- src/modules/trackmania/utils/useTelemetryInputStyle.ts
+++ src/modules/trackmania/utils/useTelemetryInputStyle.ts
@@ -7,13 +7,17 @@ function useTelemetryInputStyle(value = 0, color: string = theme.colors.orange[5
   // Find the proper HSL lightness based on max-val/min-val percentage:
   // (maxVal - minVal) * percentage + minVal
   const telemetryStyle = React.useMemo(() => {
-    if (color && isValidHex(color)) {
-      const colorHsl = parseToHsl(color)
+    if (color) {
+      if (isValidHex(color)) {
+        const colorHsl = parseToHsl(color)
 
-      return hsl(colorHsl.hue, colorHsl.saturation, colorHsl.lightness * ((0.75 - 0.25) * (value || 0 * 100) + 0.25))
+        return hsl(colorHsl.hue, colorHsl.saturation, colorHsl.lightness * ((0.75 - 0.25) * (value || 0 * 100) + 0.25))
+      }
+
+      return transparentize(Math.abs((value || 1) - 1), color)
     }
 
-    return transparentize(Math.abs((value || 1) - 1), color)
+    return color
   }, [value, color])
 
   return telemetryStyle
