fb7f87b012ad02d248539d95b08b427a7d2a88a4
Resi Respati <resir014@gmail.com>
2020-11-09 16:08:42 +0700

    use default Trackmania pad config as base defaults (#4)

---
+++ src/modules/trackmania/utils/gamepadConfigDefaults.ts
@@ -0,0 +1,19 @@
+import { UseGamepadConfig } from '~/types/gamepad'
+
+export const DEFAULT_FRAMERATE = 60
+export const DEFAULT_ACCELERATE_BUTTON = 7
+export const DEFAULT_BRAKE_BUTTON = 6
+export const DEFAULT_STEERING_AXIS = 0
+export const DEFAULT_STEERING_DEADZONE = 0.01
+
+function gamepadConfigDefaults(config?: Partial<UseGamepadConfig>): UseGamepadConfig {
+  return {
+    accelerateButton: config?.accelerateButton || DEFAULT_ACCELERATE_BUTTON,
+    brakeButton: config?.brakeButton || DEFAULT_BRAKE_BUTTON,
+    framerate: config?.framerate || DEFAULT_FRAMERATE,
+    steeringAxis: config?.steeringAxis || DEFAULT_STEERING_AXIS,
+    steeringDeadzone: config?.steeringDeadzone || DEFAULT_STEERING_DEADZONE
+  }
+}
+
+export default gamepadConfigDefaults
