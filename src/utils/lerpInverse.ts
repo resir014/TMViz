438a1ecf9bcefc1c29985dd23c1001700f5bafd3
Resi Respati <resir014@gmail.com>
2020-12-09 02:28:06 +0700

    refactor: use `@resir014/lerp` (#25)

--- src/utils/lerpInverse.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Get the delta of a number between two other numbers
- */
-export default function lerpInverse(value: number, a: number, b: number) {
-  return a === b ? 0 : (value - a) / (b - a) // prevent division by 0
-}
