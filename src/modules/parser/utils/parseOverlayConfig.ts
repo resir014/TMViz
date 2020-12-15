4fac9f9d494d7e6c3a6c4d6de1bf48d11f5d77c5
Resi Respati <resir014@gmail.com>
2020-12-15 22:59:02 +0700

    fix: bring back default steering axis

--- src/modules/parser/utils/parseOverlayConfig.ts
+++ src/modules/parser/utils/parseOverlayConfig.ts
@@ -1,5 +1,5 @@
 import { ParsedUrlQuery } from 'querystring'
-import { DEFAULT_ACCELERATE_BUTTON, DEFAULT_BRAKE_BUTTON, DEFAULT_STEERING_DEADZONE } from '~/types/constants'
+import { DEFAULT_ACCELERATE_BUTTON, DEFAULT_BRAKE_BUTTON, DEFAULT_STEERING_AXIS, DEFAULT_STEERING_DEADZONE } from '~/types/constants'
 import { TrackmaniaOverlayConfig } from '~/types/overlay'
 
 export default function parseOverlayConfig(
@@ -10,7 +10,7 @@ export default function parseOverlayConfig(
     return {
       accelerateButton: (config?.accelerateButton as string) || `${DEFAULT_ACCELERATE_BUTTON}`,
       brakeButton: (config?.brakeButton as string) || `${DEFAULT_BRAKE_BUTTON}`,
-      steeringAxis: config?.steeringAxis as string,
+      steeringAxis: (config?.steeringAxis as string) || `${DEFAULT_STEERING_AXIS}`,
       steeringDeadzone: (config?.steeringDeadzone as string) || `${DEFAULT_STEERING_DEADZONE}`,
       accelerateAxis: config?.accelerateAxis as string,
       steeringLeftButton: config?.steeringLeftButton as string,
@@ -21,7 +21,7 @@ export default function parseOverlayConfig(
   return {
     accelerateButton: (config?.accelerateButton as string) || `${DEFAULT_ACCELERATE_BUTTON}`,
     brakeButton: (config?.brakeButton as string) || `${DEFAULT_BRAKE_BUTTON}`,
-    steeringAxis: config?.steeringAxis as string,
+    steeringAxis: (config?.steeringAxis as string) || `${DEFAULT_STEERING_AXIS}`,
     steeringDeadzone: (config?.steeringDeadzone as string) || `${DEFAULT_STEERING_DEADZONE}`
   }
 }
