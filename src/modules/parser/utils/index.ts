44610d8046f2a15890d7ccdc951178532c49d148
Resi Respati <resir014@gmail.com>
2020-12-15 22:53:52 +0700

    fix: again

---
+++ src/modules/parser/utils/index.ts
@@ -0,0 +1,3 @@
+export { default as normaliseVersionNumber } from './normaliseVersionNumber'
+export { default as parseAppearance } from './parseAppearance'
+export { default as parseOverlayConfig } from './parseOverlayConfig'
