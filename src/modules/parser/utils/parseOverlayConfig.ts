65b6dd303d2868e6ec655c9a823ac9df87b96c78
Resi Respati <resir014@gmail.com>
2021-01-04 02:28:56 +0700

    fix: remove fallback on steering axis (#34)
    When steering axis is not set, the overlay shouldn't fallback to axis 0.
    
    Fixes #31.

--- src/modules/parser/utils/parseOverlayConfig.ts
+++ src/modules/parser/utils/parseOverlayConfig.ts
@@ -1,5 +1,5 @@
 import { ParsedUrlQuery } from 'querystring'
-import { DEFAULT_ACCELERATE_BUTTON, DEFAULT_BRAKE_BUTTON, DEFAULT_STEERING_AXIS, DEFAULT_STEERING_DEADZONE } from '~/types/constants'
+import { DEFAULT_ACCELERATE_BUTTON, DEFAULT_BRAKE_BUTTON, DEFAULT_STEERING_DEADZONE } from '~/types/constants'
 import { TrackmaniaOverlayConfig } from '~/types/overlay'
 
 export default function parseOverlayConfig(
@@ -10,7 +10,7 @@ export default function parseOverlayConfig(
     return {
       accelerateButton: (config?.accelerateButton as string) || `${DEFAULT_ACCELERATE_BUTTON}`,
       brakeButton: (config?.brakeButton as string) || `${DEFAULT_BRAKE_BUTTON}`,
-      steeringAxis: (config?.steeringAxis as string) || `${DEFAULT_STEERING_AXIS}`,
+      steeringAxis: config?.steeringAxis as string,
       accelerateAxis: config?.accelerateAxis as string,
       steeringLeftButton: config?.steeringLeftButton as string,
       steeringRightButton: config?.steeringRightButton as string,
@@ -22,7 +22,7 @@ export default function parseOverlayConfig(
   return {
     accelerateButton: (config?.accelerateButton as string) || `${DEFAULT_ACCELERATE_BUTTON}`,
     brakeButton: (config?.brakeButton as string) || `${DEFAULT_BRAKE_BUTTON}`,
-    steeringAxis: (config?.steeringAxis as string) || `${DEFAULT_STEERING_AXIS}`,
+    steeringAxis: config?.steeringAxis as string,
     steeringDeadzone: (config?.steeringDeadzone as string) || `${DEFAULT_STEERING_DEADZONE}`,
     controllerIndex: (config?.controllerIndex as string) || '0'
   }
