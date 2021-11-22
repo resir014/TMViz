0639d22613b7cadc1eaac71a9d779bf174162911
Resi Respati <resir014@gmail.com>
2021-11-22 22:01:25 +0700

    chore: use new Sentry Next.js plugin (#54)

--- next.config.js
+++ next.config.js
@@ -1,58 +1,28 @@
 /* eslint-disable no-param-reassign */
-// Use the hidden-source-map option when you don't want the source maps to be
-// publicly available on the servers, only to the error reporting
-const withSourceMaps = require('@zeit/next-source-maps')();
-// Use the SentryWebpack plugin to upload the source maps during build step
-const SentryWebpackPlugin = require('@sentry/webpack-plugin');
+const { withSentryConfig } = require('@sentry/nextjs');
 
-const {
-  NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
-  SENTRY_ORG,
-  SENTRY_PROJECT,
-  SENTRY_AUTH_TOKEN,
-  NODE_ENV,
-  VERCEL_GIT_COMMIT_SHA,
-} = process.env;
-
+const { VERCEL_GIT_COMMIT_SHA } = process.env;
 const COMMIT_SHA = VERCEL_GIT_COMMIT_SHA ? VERCEL_GIT_COMMIT_SHA.substring(0, 8) : undefined;
 
-process.env.SENTRY_DSN = SENTRY_DSN;
-const basePath = '';
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
 
-module.exports = withSourceMaps({
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+const nextConfig = {
   env: {
     // Make the COMMIT_SHA available to the client so that Sentry events can be
     // marked for the release they belong to. It may be undefined if running
     // outside of Vercel
-    NEXT_PUBLIC_COMMIT_SHA: COMMIT_SHA,
+    SENTRY_RELEASE: `tmviz@${COMMIT_SHA}`,
   },
-  webpack: config => {
-    // When all the Sentry configuration env variables are available/configured
-    // The Sentry webpack plugin gets pushed to the webpack plugins to build
-    // and upload the source maps to sentry.
-    // This is an alternative to manually uploading the source maps
-    // Note: This is disabled in development mode.
-    if (
-      SENTRY_DSN &&
-      SENTRY_ORG &&
-      SENTRY_PROJECT &&
-      SENTRY_AUTH_TOKEN &&
-      COMMIT_SHA &&
-      NODE_ENV === 'production'
-    ) {
-      config.plugins.push(
-        new SentryWebpackPlugin({
-          include: '.next',
-          ignore: ['node_modules'],
-          stripPrefix: ['webpack://_N_E/'],
-          urlPrefix: `~${basePath}/_next`,
-          release: COMMIT_SHA,
-        }),
-      );
-    }
-    return config;
-  },
-  basePath,
   async redirects() {
     return [
       {
@@ -72,4 +42,8 @@ module.exports = withSourceMaps({
       },
     ];
   },
-});
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
