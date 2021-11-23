e28425ee1cabf824b792827030d3cadf41e2f094
Resi Respati <resir014@gmail.com>
2021-11-24 01:27:22 +0700

    chore: remove unnecessary eslint comment

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 // This file configures the initialization of Sentry on the browser.
 // The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
