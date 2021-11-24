81e0ca177f6aa1c57968b480b0048eeb626aee21
Resi Respati <resir014@gmail.com>
2021-11-24 18:43:47 +0700

    perf: deep equal check all gamepad hooks

--- src/modules/gamepad/gamepad-hooks.ts
+++ src/modules/gamepad/gamepad-hooks.ts
@@ -1,11 +1,19 @@
+import { dequal } from 'dequal';
+import * as React from 'react';
 import { useGamepadStore } from '.';
 
 export function useGamepad() {
-  const gamepads = useGamepadStore(state => state);
+  const gamepads = useGamepadStore(
+    state => state,
+    (prevState, newState) => dequal(prevState, newState),
+  );
   return gamepads;
 }
 
-export function useIsGamepadActive(currentController: number) {
-  const current = useGamepadStore(state => state[currentController]);
+export function useIsGamepadActive(index: number) {
+  const current = useGamepadStore(
+    React.useCallback(state => state[index], [index]),
+    (prevState, newState) => dequal(prevState, newState),
+  );
   return !!current;
 }
--- src/modules/trackmania/utils/use-current-controller.ts
+++ src/modules/trackmania/utils/use-current-controller.ts
@@ -1,9 +1,16 @@
+import * as React from 'react';
+import { dequal } from 'dequal';
 import { useOverlayConfig } from './use-overlay-config';
 import { useGamepadStore } from '~/modules/gamepad';
 import { parseNumber } from '~/utils/query-parser';
 
 export function useCurrentController() {
   const { config } = useOverlayConfig();
-  const currentController = parseNumber(config.controllerIndex) ?? 0;
-  return useGamepadStore(state => state[currentController]);
+  return useGamepadStore(
+    React.useCallback(
+      state => state[parseNumber(config.controllerIndex) ?? 0],
+      [config.controllerIndex],
+    ),
+    (prevState, newState) => dequal(prevState, newState),
+  );
 }
