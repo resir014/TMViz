093dfb0c304c7b0b957a89aa51fb70737078eb71
Resi Respati <resir014@gmail.com>
2023-10-01 15:21:44 +0700

    fix: add protocol to vercel URL

--- src/modules/customizer/utils/build-url-query.ts
+++ src/modules/customizer/utils/build-url-query.ts
@@ -1,7 +1,8 @@
 import queryString from 'query-string';
 import { GlobalOverlaySettings } from '~/types/overlay';
 
-const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL ?? process.env.NEXT_PUBLIC_BASE_URL ?? '';
+const BASE_URL =
+  process.env.NEXT_PUBLIC_VERCEL_URL ?? `https://${process.env.NEXT_PUBLIC_BASE_URL}` ?? '';
 
 function buildURLQuery(settings: GlobalOverlaySettings, baseUrl: string = BASE_URL) {
   const query = { version: settings.version, ...settings.appearance, ...settings.config };
