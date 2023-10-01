3727e6fdf0ac045258f1722721256a498b934e40
Resi Respati <resir014@gmail.com>
2023-10-01 15:17:09 +0700

    fix: preview url dings

--- src/modules/customizer/utils/build-url-query.ts
+++ src/modules/customizer/utils/build-url-query.ts
@@ -1,7 +1,7 @@
 import queryString from 'query-string';
 import { GlobalOverlaySettings } from '~/types/overlay';
 
-const BASE_URL = process.env.VERCEL_URL ?? 'https://tmviz.vercel.app';
+const BASE_URL = process.env.VERCEL_URL ?? '';
 
 function buildURLQuery(settings: GlobalOverlaySettings, baseUrl: string = BASE_URL) {
   const query = { version: settings.version, ...settings.appearance, ...settings.config };
